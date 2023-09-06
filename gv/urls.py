5db1bd495b6daeaa1535251939fe45cdf3041ce0
SankojuRamesh <visitrameshs@gmail.com>
2023-09-06 19:35:29 +0530

    frontend

--- gv/urls.py
+++ gv/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from productmanager import views as pviews
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('admin/', include('adminmanager.urls')),
+    path('',  pviews.home),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
